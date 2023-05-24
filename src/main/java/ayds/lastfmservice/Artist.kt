cc0d039aaca7d5543c077f7e4342eb0333335bca
Nicolás Berti <71272720+nicolasberti@users.noreply.github.com>
2023-05-24 09:42:37 -0300

    Update Artist.kt

--- src/main/java/ayds/lastfmservice/Artist.kt
+++ src/main/java/ayds/lastfmservice/Artist.kt
@@ -5,6 +5,7 @@ sealed class Artist {
         val name: String,
         val info: String,
         val url: String,
+        val urlImageLastFM: String = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
     ) : Artist()
 
 }
